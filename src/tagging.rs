2bbe42cd792c0fa45a5d340eeed80df56b908dd3
Pierre Dal-Pra <pdalpra@mediarithmics.com>
2020-11-04 14:41:44 +0100

    Replace metaflac by audiotags

--- src/tagging.rs
+++ src/tagging.rs
@@ -1,32 +1,34 @@
 use anyhow::*;
-use metaflac::Tag;
+use audiotags::{Album, Tag};
 use std::path::PathBuf;
 
-pub fn tag_file(target_file: &PathBuf) -> Result<()> {
-    let parent = parent_directory(&target_file)?;
+pub fn tag_file(target_path: &PathBuf) -> Result<()> {
+    let parent = parent_directory(&target_path)?;
     let parent_parent = parent_directory(&parent)?;
     let album = extract_file_name(&parent)?;
     let artist = extract_file_name(&parent_parent)?;
-    let (track_number, title) = extract_track_info(target_file)?;
+    let (track_number, title) = extract_track_info(target_path)?;
 
-    let mut tag = Tag::new();
-    let comment = tag.vorbis_comments_mut();
-    comment.set_album(vec![album]);
-    comment.set_artist(vec![artist]);
-    comment.set_title(vec![title]);
-    comment.set_track(track_number);
-    tag.write_to_path(target_file)?;
+    let mut tag = Tag::new().read_from_path(target_path)?;
+    tag.set_album(Album::with_title(&album).and_artist(&artist));
+    tag.set_title(&title);
+    tag.set_track_number(track_number);
+
+    let path_as_str = target_path
+        .to_str()
+        .ok_or(anyhow!("Failed to convert {:?} to a string"))?;
+    tag.write_to_path(path_as_str)?;
 
     Ok(())
 }
 
-fn extract_track_info(path: &PathBuf) -> Result<(u32, String)> {
+fn extract_track_info(path: &PathBuf) -> Result<(u16, String)> {
     let file_name = extract_file_name(path)?;
     let space_idx = file_name
         .find(" ")
         .ok_or(anyhow!("Failed to find a space character in {:?}", path))?;
     let (track_number, title) = file_name.split_at(space_idx);
-    let track_number: u32 = track_number
+    let track_number: u16 = track_number
         .parse()
         .map_err(|err| anyhow!("Failed to extract track number from {:?}: {}", path, err))?;
     let title = title.trim().to_string();
